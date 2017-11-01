2eae57037cac84023a883ab172c60ead4486b540
Adrienne Yao <adrienne.yao@icloud.com>
2017-11-01 18:24:49 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
 
   myArray = sentence.split
   tempArray = []
-  myArray.each do |word|
-    tempArray << word.reverse
+  myArray.collect do |word|
+    word.reverse.join(" ")
   end
-  tempArray.join(" ")
+  
 end
